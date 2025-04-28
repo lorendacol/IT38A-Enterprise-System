1e6ee40aa95c39f08400085765b63da0282e797f
lorendacol <lorendacol>
2025-04-28 15:24:44 +0800

    added homepage

---
+++ Dashboard/home.php
@@ -0,0 +1,8 @@
+
+<section class="home-section">
+    <div class="welcome-banner">
+        <h1>Welcome to Sports Club</h1>
+        <p>Your Hub for Sports & Activities!</p>
+        <a href="?page=equipment" class="btn">Borrow Now!</a>
+    </div>
+</section>
