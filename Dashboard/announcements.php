69d9af2195366d669b0aff6e0d6b24eca1f2ad28
lorendacol <lorendacol>
2025-04-28 15:25:31 +0800

    added the announcement page

---
+++ Dashboard/announcements.php
@@ -0,0 +1,15 @@
+
+<section class="announcements-section">
+    <h2>Upcoming Tryouts for Basketball Team</h2>
+    <p><em>for BUACS</em></p>
+
+    <div class="announcement-content">
+        <div class="text">
+            <p>Body text for your whole article or post. We’ll put in some lorem ipsum to show how a filled-out page might look:</p>
+            <p>Excepteur efficient emerging, minim veniam anim aute carefully curated Ginza conversation exquisite perfect nostrud nisi intricate Content...</p>
+        </div>
+        <div class="image">
+            <img src="tryouts.jpg" alt="Basketball Tryouts Poster" style="max-width: 100%;">
+        </div>
+    </div>
+</section>
